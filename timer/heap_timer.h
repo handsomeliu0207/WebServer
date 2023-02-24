2c9b4292c32fab234f755e2c15cfdd87854af365
ubuntu <ubuntu@localhost.localdomain>
2023-02-24 15:32:48 +0800

    对信号相关函数进行封装

--- timer/heap_timer.h
+++ timer/heap_timer.h
@@ -1,14 +1,30 @@
 #ifndef HEAP_TIMER_H
 #define HEAP_TIMER_H
 
-#include <stdio.h>
-#include <time.h>
-#include <arpa/inet.h>
-#include <queue>
-#include <vector>
-#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
 #include <sys/epoll.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <queue>
+
+#include <time.h>
 #include <memory>
+
 
 class heap_timer;
 
@@ -53,7 +69,36 @@ private:
     std::priority_queue<std::shared_ptr<heap_timer>, std::vector<std::shared_ptr<heap_timer>>, timer_cmp> heap;
 };
 
+class Utils
+{
+public:
+    Utils() {}
+    ~Utils() {}
 
+    void init(int timeslot);
 
+    //对文件描述符设置非阻塞
+    int setnonblocking(int fd);
+
+    //将内核事件表注册读事件，ET模式，选择开启EPOLLONESHOT
+    void addfd(int epollfd, int fd, bool one_shot, int TRIGMode);
+
+    //信号处理函数
+    static void sig_handler(int sig);
+
+    //设置信号函数
+    void addsig(int sig, void(handler)(int), bool restart = true);
+
+    //定时处理任务，重新定时以不断触发SIGALRM信号
+    void timer_handler();
+
+    void show_error(int connfd, const char *info);
+
+public:
+    static int *u_pipefd;
+    time_heap m_time_heap;
+    static int u_epollfd;
+    int m_TIMESLOT;
+};
 
 #endif
